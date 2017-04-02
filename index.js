773bb8e3e1fa682873f0cf9075c064f759a361c3
Jose Manuel <josemanuel.crv@gmail.com>
2017-04-02 19:45:13 +0200

    Added functionality to save on mongodb with POST using mongoose driver

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product = require('./models/product')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -30,8 +33,22 @@ app.get('/api/product/:productId', (req, res) =>{
 
 //
 app.post('/api/product', (req, res) =>{
-	console.log(req.body);
-	res.status(200).send({message: 'El producto se ha recibido'})
+	console.log('POST /api/product')
+	var reqBody = req.body
+	console.log(reqBody)
+
+	let product = new Product()
+	product.name = req.body.name
+	product.price = req.body.price
+	product.picture = req.body.picture
+	product.category = req.body.category
+	product.description = req.body.description
+
+	product.save((err, productStored) =>{
+		if(err) res.status(500).send({message: `Error al salvar la BD: ${err}`})
+
+		res.status(200).send({product: productStored})
+	})
 	
 })
 
@@ -45,8 +62,19 @@ app.delete('/api/product/:productId', (req, res) =>{
 	
 })
 
-//
-app.listen(port, () => {
-  console.log(`API REST corriendo en http://localhost:${port}`);
+
+mongoose.connect('mongodb://localhost:27017/shop', (err, res)=>{
+	if(err){
+		return console.log(`Error al conectar a la BD: ${err}`)	
+	}
+	console.log('Conexión a la BD establecida...')	
+
+	// Listen Server connex
+	app.listen(port, () => {
+	  console.log(`API REST corriendo en http://localhost:${port}`);
+	})
+
 })
+
+
 
